8119862fbdc8246cbb1bf9c3dae8d68fa011e0ed
Erwan-Vinot <pro.vinot.erwan@gmail.com>
2023-10-23 14:52:31 +0200

    feat(lecture-CSV): Ajoute la lecture du fichier CSV 'noms_m1.csv' pour obtenir les noms/prénoms des étudiants

--- classe.py
+++ classe.py
@@ -1,3 +1,11 @@
+import csv
 import random
 
-def
+# Lecture du CSV contenant les noms/ prénoms
+students = []
+with open('data/noms_m1.csv', 'r', encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=';')
+    for row in csvreader:
+        students.append({'firstname': row[0], 'lastname': row[1]})
+
+print(students)
