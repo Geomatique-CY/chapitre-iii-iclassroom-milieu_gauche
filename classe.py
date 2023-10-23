cc3b3802182c58c3eaa6b1505a720e9a9fbb5a55
cgambart27 <gambart.c95@gmail.com>
2023-10-23 16:23:43 +0200

    code corrigé

--- classe.py
+++ classe.py
@@ -26,20 +26,24 @@ variable = len (students)
 print(variable)
 if 'lastname ' in students :
     students.remove('lastname ')
-n=1
+n = 0
 
+# créer un groupe de 3 personnes :
 def groupe3(n, groupe) :
     for u in range(3) : 
         groupe += students[n]['lastname']+ ' '
         n+=1
     return(groupe)
 
+#créer un groupe de 2 personnes :
 def groupe2(n, groupe) : 
     for u in range(2) : 
         groupe += students[n]['lastname']+ ' '
         n+=1
     return(groupe)
-for i in range (3) :
+
+#pour les 2 premières rangées, créer un groupe de 3, puis de 2, puis de 3 : 
+for i in range (2) :
     g1=''
     g1 = groupe3(n, g1)
     n+=3
@@ -51,4 +55,19 @@ for i in range (3) :
     g3=''
     g3 = groupe3(n, g3)
     n+=3
-    print(g1 + '        ' + g2 + '       ' + g3)
+    print(g1 + '    /   ' + g2 + '    /   ' + g3)
+
+#pour la dernière ligne : 1 groupe de 3 puis 2 groupes de 2
+
+g1=''
+g1 = groupe3(n, g1)
+n+=3
+
+g2 = ''
+g2 = groupe2(n, g2)
+n+=2
+
+g3=''
+g3 = groupe2(n, g3)
+n+=2
+print(g1 + '    /   ' + g2 + '    /   ' + g3)
