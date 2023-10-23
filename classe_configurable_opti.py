347ec4476a9c103d705935dd1c3080b2bd1e8fc8
Erwan-Vinot <pro.vinot.erwan@gmail.com>
2023-10-23 21:46:00 +0200

    refactor: Simplification de classe_configurable_opti.py et correction du formatage

--- classe_configurable_opti.py
+++ classe_configurable_opti.py
@@ -1,12 +1,9 @@
 import csv,random
 
-nombres_lignes, nombres_groupes_par_ligne,nombres_etudiants = 3, 3, {"groupe gauche": 3, "groupe milieu": 2, "groupe droit": 3}
+with open('data/noms_m1.csv', 'r', encoding='utf-8') as csvfile: students = [row[1] for row in csv.reader(csvfile, delimiter=';')] 
 
-with open('data/noms_m1.csv', 'r', encoding='utf-8') as csvfile: students = [row[1] for row in csv.reader(csvfile, delimiter=';')]
-
-random.shuffle(students)
-eleve = lambda: students.pop() if students else "Personne"
-
-classe = [{f"ligne{i+1}": {nom: [eleve() for _ in range(nombres)] for nom, nombres in nombres_etudiants.items()}}for i in range(nombres_lignes)]
+nombres_lignes, nombres_groupes_par_ligne, nombres_etudiants = 3, 3, {"groupe gauche": 3, "groupe milieu": 2, "groupe droit ": 3} 
+eleve = lambda: students.pop((random.randrange(len(students)))) if students else "Personne" 
+classe = [{f"Rangée {i+1}": {nom: [eleve() for _ in range(nombres)] for nom, nombres in nombres_etudiants.items()}}for i in range(nombres_lignes)]
 
 [print(f"\n{rang} :\n" + '\n'.join([f"  {nom_groupe} : {etudiants}" for nom_groupe, etudiants in groupes.items()])) for ligne in classe for rang, groupes in ligne.items()]
